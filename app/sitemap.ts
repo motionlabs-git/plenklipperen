8ad1e85a5fd25bafd72c08bce7c22163606319fa
jindrahabarta <jindriskuv@email.cz>
2026-02-10 18:59:28 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,20 @@
 const sitemap = () => {
-	return null
+	const baseURL = 'https://acme.com'
+
+	return [
+		{
+			url: baseURL,
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 1,
+		},
+		{
+			url: `${baseURL}/about`,
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+	]
 }
 
 export default sitemap
