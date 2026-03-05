92a79b20cffbf3b5583bebeef739b48014253ae5
jindrahabarta <jindriskuv@email.cz>
2026-03-05 19:04:06 +0100

    site ver

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export const metadata: Metadata = {
 		},
 	},
 	verification: {
-		google: 'zUuHMhenAqauIr2rNk1JmfK-_GsLAhWf5WuLgu_TpS4',
+		google: 'u8k11qOfcb8ezsXd-huKILV4GvBBQrKKEs_y0GVT8sY',
 	},
 }
 
